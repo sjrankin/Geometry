4e96ad658bd1caa4c9cf0fe1507d6b297dfa6122
Stuart Rankin <sjrankin@gmail.com>
2021-10-20 17:29:44 -0700

    Update ViewController.swift
    Updated for separate loop testing control.

--- Geometry/ViewController.swift
+++ Geometry/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         DrawSurface.layer.cornerRadius = 5.0
         DrawSurface.layer.borderColor = UIColor.gray.cgColor
         DrawSurface.layer.borderWidth = 1.0
+        DrawSurface.layer.zPosition = 1000
+        DrawSurface.isUserInteractionEnabled = true
         PointPicker.layer.cornerRadius = 5.0
         PointPicker.layer.borderColor = UIColor.gray.cgColor
         PointPicker.layer.borderWidth = 1.0
@@ -24,6 +26,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         LoopDataContainer.layer.borderWidth = 1.0
         LoopDataContainer.layer.cornerRadius = 5.0
         LoopDataContainer.isHidden = true
+        LoopDrawSurface.layer.cornerRadius = 5.0
+        LoopDrawSurface.layer.borderColor = UIColor.gray.cgColor
+        LoopDrawSurface.layer.borderWidth = 1.0
+        LoopDrawSurface.layer.zPosition = -1000
+        LoopDrawSurface.isUserInteractionEnabled = false
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
@@ -53,8 +60,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         if !DidInitialSetup
         {
             DidInitialSetup = true
-            DrawSurface.Initialize(FromSurface, NewPoint)
+            DrawSurface.Initialize(FromSurface)
             DrawSurface.PlotSurface()
+            
+            LoopDrawSurface.Initialize(LoopDataCallback: NewPoint, FromSurface)
+            LoopDrawSurface.PlotSurface()
         }
     }
     
@@ -90,7 +100,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     var DidInitialSetup = false
     var GridGap = 16
-
+    
     @IBAction func DecorateButtonHandler(_ sender: Any)
     {
         DrawSurface.Decorate = !DrawSurface.Decorate
@@ -106,6 +116,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         DrawSurface.TestPoint = nil
         DrawSurface.RotateFrame = false
         DrawSurface.PlotSurface()
+        
+        LoopDrawSurface.OriginalPoints.removeAll()
+        LoopDrawSurface.PlotSurface()
     }
     
     @IBAction func RotateButtonHandler(_ sender: Any)
@@ -121,34 +134,134 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         DrawSurface.PlotSurface()
     }
     
-    @IBAction func InsertLoopHandler(_ sender: Any)
+    var InLoopMode: Bool = false
+    
+    @IBAction func ModeButtonHandler(_ sender: Any)
     {
+        InLoopMode = !InLoopMode
+        let Title = InLoopMode ? "3 Point Mode" : "Loop Mode"
+        LoopModeButton.setTitle(Title, for: .normal)
+        DrawSurface.isUserInteractionEnabled = !InLoopMode
+        DrawSurface.layer.zPosition = InLoopMode ? -1000 : 1000
+        LoopDrawSurface.isUserInteractionEnabled = InLoopMode
+        LoopDrawSurface.layer.zPosition = InLoopMode ? 1000 : -1000
+        
+        if InLoopMode
+        {
+            LoopDrawSurface.MLocation = nil
+            LoopDrawSurface.MAngle = nil
+            LoopDrawSurface.MClosest = nil
+            LoopDrawSurface.OriginalPoints.removeAll()
+            LoopDrawSurface.PlotSurface()
+        }
+        else
+        {
         ClearButtonHandler(sender)
         ClearNewPointData()
-        DrawSurface.InLoopMode = !DrawSurface.InLoopMode
-        PointPicker.isHidden = DrawSurface.InLoopMode
-        LoopDataContainer.isHidden = !DrawSurface.InLoopMode
-        if !DrawSurface.InLoopMode
+            DrawSurface.OriginalPoints.removeAll()
+        PointPicker.isHidden = InLoopMode
+        LoopDataContainer.isHidden = !InLoopMode
+            DrawSurface.PlotSurface()
+        }
+    }
+    
+    @IBAction func ShapeSegmentChangeHandler(_ sender: Any)
+    {
+        if !InLoopMode
         {
+            print("Not in loop mode")
             return
         }
-        OriginalPoints.removeAll()
-#if true
-        OriginalPoints.append(CGPoint(x: 0, y: 150))
-        OriginalPoints.append(CGPoint(x: -150, y: -150.0))
-        OriginalPoints.append(CGPoint(x: 150, y: -150.0))
-        #else
-        OriginalPoints.append(CGPoint(x: -150, y: -150))
-        OriginalPoints.append(CGPoint(x: 150, y: -150))
-        OriginalPoints.append(CGPoint(x: 150, y: 150))
-        OriginalPoints.append(CGPoint(x: -150, y: 150))
-        #endif
-        DrawSurface.OriginalPoints = OriginalPoints
-        DrawSurface.PlotSurface()
+        let CenterX = LoopDrawSurface.frame.size.width / 2.0
+        let CenterY = LoopDrawSurface.frame.size.height / 2.0
+        let Center = CGPoint(x: CenterX, y: CenterY)
+        switch SideCount.selectedSegmentIndex
+        {
+            case 0: //3 sides
+                OriginalPoints = RadialPoints(Center: Center, Radius: 150.0, Count: 3)
+                
+            case 1: //4 sides
+                OriginalPoints = RadialPoints(Center: Center, Radius: 150.0, Count: 4)
+                
+            case 2: //4 sides rotated 90°
+                OriginalPoints.removeAll()
+                OriginalPoints.append(CGPoint(x: CenterX - 150, y: CenterY - 150))
+                OriginalPoints.append(CGPoint(x: CenterX + 150, y: CenterY - 150))
+                OriginalPoints.append(CGPoint(x: CenterX + 150, y: CenterY + 150))
+                OriginalPoints.append(CGPoint(x: CenterX - 150, y: CenterY + 150))
+                
+            case 3: //5 sides
+                OriginalPoints = RadialPoints(Center: Center, Radius: 150.0, Count: 5)
+                
+            case 4: //6 sides
+                OriginalPoints = RadialPoints(Center: Center, Radius: 150.0, Count: 6)
+                
+            case 5: //7 sides
+                OriginalPoints = RadialPoints(Center: Center, Radius: 150.0, Count: 7)
+                
+            case 6: //8 sides
+                OriginalPoints = RadialPoints(Center: Center, Radius: 150.0, Count: 8)
+                
+            case 7: //9 sides
+                OriginalPoints = RadialPoints(Center: Center, Radius: 150.0, Count: 9)
+                
+            case 8: //random points
+                let RCount = Int.random(in: 6 ... 10)
+                OriginalPoints.removeAll()
+                let Increment = 360.0 / CGFloat(RCount)
+                for Index in 0 ..< RCount
+                {
+                    var RRadius = 150
+                    RRadius = RRadius + Int.random(in: -10 ... 10)
+                    var Radial = Increment * CGFloat(Index)
+                    Radial = Radial + CGFloat.random(in: -Increment * 0.3 ... Increment * 0.3)
+                    Radial = Radial * CGFloat.pi / 180.0
+                    let Point = RadialPoint(Center: Center,
+                                            Radius: CGFloat(RRadius),
+                                            Radial: Radial)
+                    OriginalPoints.append(Point)
+                }
+                
+            default:
+                return
+        }
+        
+        LoopDrawSurface.OriginalPoints = OriginalPoints
+        LoopDrawSurface.PlotSurface()
+    }
+    
+    func RadialPoints(Center: CGPoint, Radius: CGFloat, Count: Int) -> [CGPoint]
+    {
+        var Points = [CGPoint]()
+        if Count < 3
+        {
+            return Points
+        }
+        let AngleIncrement = 360.0 / CGFloat(Count)
+        for Index in 0 ..< Count
+        {
+            let Degrees = AngleIncrement * CGFloat(Index)
+            let Radians = Degrees * CGFloat.pi / 180.0
+            let X = Radius * cos(Radians)
+            let Y = Radius * sin(Radians)
+            let Point = CGPoint(x: X + Center.x, y: Y + Center.y)
+            Points.append(Point)
+        }
+        return Points
+    }
+    
+    func RadialPoint(Center: CGPoint, Radius: CGFloat, Radial: CGFloat) -> CGPoint
+    {
+        let X = Radius * cos(Radial)
+        let Y = Radius * sin(Radial)
+        let Point = CGPoint(x: X + Center.x, y: Y + Center.y)
+        return Point
     }
     
     var OriginalPoints = [CGPoint]()
     
+    @IBOutlet weak var LoopModeButton: UIButton!
+    @IBOutlet weak var SideCount: UISegmentedControl!
     @IBOutlet weak var NewToP1Label: UILabel!
     @IBOutlet weak var NewToM1Label: UILabel!
     @IBOutlet weak var InbetweenLabel: UILabel!
@@ -160,6 +273,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var CallbackLabel: UILabel!
     @IBOutlet weak var PointPicker: UIPickerView!
     @IBOutlet weak var DrawSurface: SurfaceWindow!
+    @IBOutlet weak var LoopDrawSurface: LoopSurfaceWindow!
 }
 
 enum PointTypes: String, CaseIterable
